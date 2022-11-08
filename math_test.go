2e1b103a1bc3d178a3e03c3a59cbcf074a8a54a5
PauloMenegasso <paulomen.pm@gmail.com>
2022-11-08 09:04:34 -0300

    feat: add sum

---
+++ math_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+	firstArgument := 15
+	secondArgument := 15
+
+	total := Soma(firstArgument, secondArgument)
+	expectedValue := firstArgument + secondArgument
+
+	if total != expectedValue {
+		t.Errorf("Valor esperado e valor encontrado não são iguais. Valor encontrado: %d, valor esperado: %d", total, expectedValue)
+	}
+}
